05494c4f88d430be34c7282d176c99e3d8b84530
Zaminali <2g17rck@code.eddu.az>
2022-12-30 03:51:25 +0400

    HomeWork

---
+++ EternaProject/EternaProject/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EternaProject.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<Clients> Clients { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<PortfolioImage> PortfolioImages { get; set; }
+    }
+}
